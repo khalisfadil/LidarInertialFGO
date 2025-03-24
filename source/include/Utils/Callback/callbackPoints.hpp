e9b2610a1b309b1f4c83540f207be3787cfa91d2
Khalis Fadil <khalis_muse@yahoo.com>
2025-03-24 15:21:00 +0100

    update

--- source/include/Utils/Callback/callbackPoints.hpp
+++ source/include/Utils/Callback/callbackPoints.hpp
@@ -47,13 +47,14 @@ namespace slam {
                 const uint32_t temp_segm = *reinterpret_cast<const uint32_t*>(&data[13]);
                 const uint32_t temp_frameID = *reinterpret_cast<const uint32_t*>(&data[65]);
                 const uint32_t temp_numInput = *reinterpret_cast<const uint32_t*>(&data[69]);
-                std::cout << "[startPointsListener1]: " << receivedNumInput_ << "\n";
+                std::cout << "[startPointsListener1]: " << temp_frameID << "\n";
                 if (data.size() != 73 + temp_numInput * 24) return;
 
                 if (temp_frameID != frameID_) {
                     if (maxNumSegment_ == currSegmIdx_ - 1) {
-                        points.pt.assign(receivedPt_.begin(), receivedPt_.begin() + receivedNumInput_);
-                        points.att.assign(receivedAtt_.begin(), receivedAtt_.begin() + receivedNumInput_);
+                        std::copy(receivedPt_.begin(), receivedPt_.begin() + receivedNumInput_, points.pt.begin());
+                        std::copy(receivedAtt_.begin(), receivedAtt_.begin() + receivedNumInput_, points.att.begin());
+
                         points.numInput = receivedNumInput_;
                         points.frameID = frameID_;
                         points.t = t_;
@@ -68,60 +69,29 @@ namespace slam {
                     maxNumSegment_ = temp_maxSegm;
                     currSegmIdx_ = 0;
                 }
-
-                currSegmIdx_++;
-                const uint32_t temp_offset = temp_segm * 57;
-                if (temp_offset + temp_numInput > MAX_NUM_POINT) return;
-
-                const float* pointData = reinterpret_cast<const float*>(&data[73]);
-                if (temp_numInput >= 16) {
-                    tbb::parallel_for(tbb::blocked_range<uint32_t>(0, temp_numInput, 4),
-                        [&](const tbb::blocked_range<uint32_t>& r) {
-                            for (uint32_t i = r.begin(); i < r.end(); i += 4) {
-                                if (i + 3 < r.end()) {
-                                    // Load 24 floats (4 points × 6 floats)
-                                    __m256 p1 = _mm256_loadu_ps(pointData + i * 6);     // x1, y1, z1, ax1, ay1, az1, x2, y2
-                                    __m256 p2 = _mm256_loadu_ps(pointData + i * 6 + 8); // z2, ax2, ay2, az2, x3, y3, z3, ax3
-                                    __m256 p3 = _mm256_loadu_ps(pointData + i * 6 + 16);// ay3, az3, x4, y4, z4, ax4, ay4, az4
-
-                                    // Convert to double
-                                    __m256d pt1 = _mm256_cvtps_pd(_mm256_extractf128_ps(p1, 0)); // x1, y1, z1, ax1
-                                    __m256d pt2 = _mm256_cvtps_pd(_mm256_extractf128_ps(p2, 0)); // z2, ax2, ay2, az2
-                                    __m256d pt3 = _mm256_cvtps_pd(_mm256_extractf128_ps(p3, 0)); // ay3, az3, x4, y4
-
-                                    __m256d att1 = _mm256_cvtps_pd(_mm256_extractf128_ps(p1, 1)); // ay1, az1, x2, y2
-                                    __m256d att2 = _mm256_cvtps_pd(_mm256_extractf128_ps(p2, 1)); // x3, y3, z3, ax3
-                                    __m256d att3 = _mm256_cvtps_pd(_mm256_extractf128_ps(p3, 1)); // z4, ax4, ay4, az4
-
-                                    // Store
-                                    _mm256_storeu_pd(receivedPt_[temp_offset + i].data(), pt1);
-                                    _mm256_storeu_pd(receivedPt_[temp_offset + i + 1].data() + 2, pt2); // Offset for z
-                                    _mm256_storeu_pd(receivedPt_[temp_offset + i + 2].data() + 1, pt3); // Offset for y,z
-
-                                    _mm256_storeu_pd(receivedAtt_[temp_offset + i].data() + 1, att1);    // Offset for y,z
-                                    _mm256_storeu_pd(receivedAtt_[temp_offset + i + 1].data(), att2);
-                                    _mm256_storeu_pd(receivedAtt_[temp_offset + i + 2].data() + 2, att3); // Offset for z
-                                } else {
-                                    for (; i < r.end(); ++i) {
-                                        const float* base = pointData + i * 6;
-                                        receivedPt_[temp_offset + i] = Eigen::Map<const Eigen::Vector3f, Eigen::Unaligned>(base).cast<double>();
-                                        receivedAtt_[temp_offset + i] = Eigen::Map<const Eigen::Vector3f, Eigen::Unaligned>(base + 3).cast<double>();
-                                    }
-                                }
-                            }
-                        }, tbb::simple_partitioner());
-                } else {
-                    for (uint32_t i = 0; i < temp_numInput; ++i) {
-                        const float* base = pointData + i * 6;
-                        receivedPt_[temp_offset + i] = Eigen::Map<const Eigen::Vector3f, Eigen::Unaligned>(base).cast<double>();
-                        receivedAtt_[temp_offset + i] = Eigen::Map<const Eigen::Vector3f, Eigen::Unaligned>(base + 3).cast<double>();
-                    }
-                }
-
-                receivedNumInput_ = temp_offset + temp_numInput;
                 
-                std::cout << "[startPointsListener2]: " << receivedNumInput_ << "\n";
-                std::cout << "[startPointsListener3]: " << points.numInput << "\n";
+
+                if (data.size() - 73 == temp_numInput * 24) {
+
+                    currSegmIdx_++;
+                    const uint32_t temp_offset = temp_segm * 57;
+                    
+                    for (uint32_t i = 0; i < temp_numInput; ++i) {
+                        float point[3];    // For XYZ coordinates
+                        float attrib[3];   // For attributes
+                        
+                        // Copy XYZ coordinates (first 12 bytes = 3 floats)
+                        std::memcpy(point, &data[73 + (i * 12)], sizeof(point));
+                        receivedPt_[temp_offset + i] = Eigen::Vector3d(point[0], point[1], point[2]);
+                        
+                        // Copy attributes (next 12 bytes = 3 floats)
+                        std::memcpy(attrib, &data[73 + (i * 12) + 12], sizeof(attrib));
+                        receivedAtt_[temp_offset + i] = Eigen::Vector3d(attrib[0], attrib[1], attrib[2]);
+                    }
+                    
+                    receivedNumInput_ = temp_offset + temp_numInput;
+                }
+                std::cout << "[startPointsListener2]: " << points.frameID << "\n";
             }
 
         private:
