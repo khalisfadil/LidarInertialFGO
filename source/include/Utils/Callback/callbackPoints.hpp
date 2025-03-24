ed46cb47943495c50fce5ae1ca5a6e16dd07a098
Khalis Fadil <khalis_muse@yahoo.com>
2025-03-24 15:26:17 +0100

    update

--- source/include/Utils/Callback/callbackPoints.hpp
+++ source/include/Utils/Callback/callbackPoints.hpp
@@ -40,56 +40,80 @@ namespace slam {
 
             // -----------------------------------------------------------------------------
             void process(const std::vector<uint8_t>& data, Points& points) {
-                if (data.size() < 73 || data[0] != 0x53) return;
-
-                const double temp_t = *reinterpret_cast<const double*>(&data[1]);
-                const uint32_t temp_maxSegm = *reinterpret_cast<const uint32_t*>(&data[9]);
-                const uint32_t temp_segm = *reinterpret_cast<const uint32_t*>(&data[13]);
-                const uint32_t temp_frameID = *reinterpret_cast<const uint32_t*>(&data[65]);
-                const uint32_t temp_numInput = *reinterpret_cast<const uint32_t*>(&data[69]);
-                std::cout << "[startPointsListener1]: " << temp_frameID << "\n";
-                if (data.size() != 73 + temp_numInput * 24) return;
-
-                if (temp_frameID != frameID_) {
-                    if (maxNumSegment_ == currSegmIdx_ - 1) {
-                        std::copy(receivedPt_.begin(), receivedPt_.begin() + receivedNumInput_, points.pt.begin());
-                        std::copy(receivedAtt_.begin(), receivedAtt_.begin() + receivedNumInput_, points.att.begin());
-
-                        points.numInput = receivedNumInput_;
-                        points.frameID = frameID_;
-                        points.t = t_;
-                        points.NED = NED_;
-                        points.RPY = RPY_;
-                    } 
-                    NED_ = Eigen::Map<const Eigen::Vector3d, Eigen::Unaligned>(reinterpret_cast<const double*>(&data[17]));
-                    RPY_ = Eigen::Map<const Eigen::Vector3d, Eigen::Unaligned>(reinterpret_cast<const double*>(&data[41]));
-                    receivedNumInput_ = 0;
-                    frameID_ = temp_frameID;
-                    t_ = temp_t;
-                    maxNumSegment_ = temp_maxSegm;
-                    currSegmIdx_ = 0;
-                }
-                
-
-                if (data.size() - 73 == temp_numInput * 24) {
-
-                    currSegmIdx_++;
-                    const uint32_t temp_offset = temp_segm * 57;
+                if (data.empty()) return;
+    
+                // Check the header byte to ensure data packet integrity
+                if (data[0] == 0x53) { // Byte 0: Header byte
                     
-                    for (uint32_t i = 0; i < temp_numInput; ++i) {
-                        float point[3];    // For XYZ coordinates
-                        float attrib[3];   // For attributes
-                        
-                        // Copy XYZ coordinates (first 12 bytes = 3 floats)
-                        std::memcpy(point, &data[73 + (i * 12)], sizeof(point));
-                        receivedPt_[temp_offset + i] = Eigen::Vector3d(point[0], point[1], point[2]);
-                        
-                        // Copy attributes (next 12 bytes = 3 floats)
-                        std::memcpy(attrib, &data[73 + (i * 12) + 12], sizeof(attrib));
-                        receivedAtt_[temp_offset + i] = Eigen::Vector3d(attrib[0], attrib[1], attrib[2]);
+                    // Extract double Timestamp (8 bytes)
+                    double temp_t;
+                    std::memcpy(&temp_t, &data[1], sizeof(double)); // Bytes 1 to 8
+
+                    // Extract uint8 MaxSegment (4 byte)
+                    uint32_t temp_maxSegm; 
+                    std::memcpy(&temp_maxSegm, &data[9], sizeof(uint32_t)); // Bytes 9 to 12
+
+                    // Extract uint8 SegmentIndex (4 byte)
+                    uint32_t temp_segm; 
+                    std::memcpy(&temp_segm, &data[13], sizeof(uint32_t)); // Bytes 13 to 16
+
+                    // Extract position and orientation (North, East, Down, Roll, Pitch, Yaw)
+                    double temp_ned[3];
+                    std::memcpy(temp_ned, &data[17], 3 * sizeof(double)); // Bytes 17 to 40
+                    double temp_rpy[3];
+                    std::memcpy(temp_rpy, &data[41], 3 * sizeof(double)); // Bytes 41 to 64
+
+                    // Extract frame metadata
+                    uint32_t temp_frameID;
+                    std::memcpy(&temp_frameID, &data[65], sizeof(uint32_t)); // Bytes 65 to 68
+
+                    uint32_t temp_numInput;
+                    std::memcpy(&temp_numInput, &data[69], sizeof(uint32_t)); // Bytes 69 to 72
+
+                    std::cout << "[startPointsListener1]: " << temp_frameID << "\n";
+                    if (data.size() != 73 + temp_numInput * 24) return;
+
+                    if (temp_frameID != frameID_) {
+                        if (maxNumSegment_ == currSegmIdx_ - 1) {
+                            std::copy(receivedPt_.begin(), receivedPt_.begin() + receivedNumInput_, points.pt.begin());
+                            std::copy(receivedAtt_.begin(), receivedAtt_.begin() + receivedNumInput_, points.att.begin());
+
+                            points.numInput = receivedNumInput_;
+                            points.frameID = frameID_;
+                            points.t = t_;
+                            points.NED = NED_;
+                            points.RPY = RPY_;
+                        } 
+                        NED_ << temp_ned[0], temp_ned[1], temp_ned[2];
+                        RPY_ << temp_rpy[0], temp_rpy[1], temp_rpy[2];
+                        receivedNumInput_ = 0;
+                        frameID_ = temp_frameID;
+                        t_ = temp_t;
+                        maxNumSegment_ = temp_maxSegm;
+                        currSegmIdx_ = 0;
                     }
                     
-                    receivedNumInput_ = temp_offset + temp_numInput;
+
+                    if (data.size() - 73 == temp_numInput * 24) {
+
+                        currSegmIdx_++;
+                        const uint32_t temp_offset = temp_segm * 57;
+                        
+                        for (uint32_t i = 0; i < temp_numInput; ++i) {
+                            float point[3];    // For XYZ coordinates
+                            float attrib[3];   // For attributes
+                            
+                            // Copy XYZ coordinates (first 12 bytes = 3 floats)
+                            std::memcpy(point, &data[73 + (i * 12)], sizeof(point));
+                            receivedPt_[temp_offset + i] = Eigen::Vector3d(point[0], point[1], point[2]);
+                            
+                            // Copy attributes (next 12 bytes = 3 floats)
+                            std::memcpy(attrib, &data[73 + (i * 12) + 12], sizeof(attrib));
+                            receivedAtt_[temp_offset + i] = Eigen::Vector3d(attrib[0], attrib[1], attrib[2]);
+                        }
+                        
+                        receivedNumInput_ = temp_offset + temp_numInput;
+                    }
                 }
                 std::cout << "[startPointsListener2]: " << points.frameID << "\n";
             }
