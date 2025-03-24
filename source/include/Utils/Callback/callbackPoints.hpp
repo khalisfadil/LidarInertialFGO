156cbe20ea6d75b5b876019f362167b8a396dba1
Khalis Fadil <khalis_muse@yahoo.com>
2025-03-24 16:54:21 +0100

    update

--- source/include/Utils/Callback/callbackPoints.hpp
+++ source/include/Utils/Callback/callbackPoints.hpp
@@ -70,6 +70,10 @@ namespace slam {
 
 
                     if (temp_frameID != frameID_) {
+
+                        std::cout << "[temp_frameID]: " << temp_frameID << "\n";
+                        std::cout << "[frameID_]: " << frameID_ << "\n";
+                        
                         if (maxNumSegment_ == currSegmIdx_ - 1) {
                             std::copy(receivedPt_.begin(), receivedPt_.begin() + receivedNumInput_, points.pt.begin());
                             std::copy(receivedAtt_.begin(), receivedAtt_.begin() + receivedNumInput_, points.att.begin());
