e3e8dfefcc73c4e8a7dede77b1cfbf903c5ba1d4
Khalis Fadil <khalis_muse@yahoo.com>
2025-03-26 17:59:09 +0100

    update

--- source/include/Utils/constants.hpp
+++ source/include/Utils/constants.hpp
@@ -4,8 +4,8 @@ namespace slam {
 
     constexpr int MAX_NUM_POINT = 128 * 1024;
 
-    constexpr size_t PARALLEL_THRESHOLD = 10000;
+    constexpr size_t PARALLEL_THRESHOLD = 1000;
 
-    constexpr int MIN_NEIGHBORS_THRESHOLD = 3;
+    constexpr int MIN_NEIGHBORS_THRESHOLD = 1;
 
 } // namespace slam
--- source/include/Utils/mapconfig.hpp
+++ source/include/Utils/mapconfig.hpp
@@ -7,15 +7,15 @@ namespace slam {
 
     struct MapConfig {
 
-        double resolution = 1.0;           ///< The resolution of the map in meters per voxel.
-        double mapMaxDistance = 250.0;   ///< The maximum distance that can be reached in the map.
+        double resolution = 0.5;           ///< The resolution of the map in meters per voxel.
+        double mapMaxDistance = 300.0;   ///< The maximum distance that can be reached in the map.
         Eigen::Vector3d mapOrigin = Eigen::Vector3d::Zero();
         unsigned int maxPointsPerVoxel = 20;
-        ColorMode colorMode = ColorMode::Occupancy;
+        ColorMode colorMode = ColorMode::Reflectivity;
 
         double tolerance = 0.1;
         size_t min_size = 3;
-        size_t max_size = 1000;
+        size_t max_size = 20;
         size_t max_frames = 5;
 
     };
@@ -23,7 +23,7 @@ namespace slam {
     struct ProcessConfig {
 
         double mapMaxDistance = 300.0;   ///< The maximum distance that can be reached in the map.
-        double mapMinDistance = 50.0;   ///< The maximum distance that can be reached in the map.
+        double mapMinDistance = 15.0;   ///< The maximum distance that can be reached in the map.
         Eigen::Vector3d mapOrigin = Eigen::Vector3d::Zero();
     };
 
